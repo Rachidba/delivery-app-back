43936162db61d58ac88ce3cd2a43de40912dfbe3
ELKAISSI SOUHAIL <52584963+souhailelk@users.noreply.github.com>
2020-01-30 00:25:22 +0100

    Add Unit Test Project & Build (#9)

---
+++ Sources/delivery-app-back/Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace delivery_app_back.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
